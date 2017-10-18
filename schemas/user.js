42f458397b8d763174c33d3447a6189c02a65137
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-10-17 20:12:54 -0400

    root query

--- schemas/user.js
+++ schemas/user.js
@@ -1,8 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
 
+const users = [
+    {id:1,firstName:'Robin',age:24},
+    {id:2,firstName:'Catherine',age:25}
+];
 const UserType = new GraphQLObjectType({
     name:'User',
     fields: {
@@ -10,4 +18,22 @@ const UserType = new GraphQLObjectType({
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt}
     }
+});
+// Permet a graphql d'entrer dans le graph de notre application
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        user:{
+            type:UserType,
+            args:{id:{type:GraphQLString}},
+            //  Ici on va récuperer les données avec les parametres en entrée
+            resolve(parentValue,args){
+                _.find(users,{id:args.id})
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
 });
